4928b88678045ce1b1ede35cc4e785efb8c3fd35
Sergey <stal722@mail.ru>
2023-07-09 22:47:24 +0300

    Создаем первый учебный тест

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTests {
+    @Test
+    void successfullLoginTest() {
+        Configuration.holdBrowserOpen = true;
+
+        open("https://qa.guru/cms/system/login/");
+        $(".login-form").shouldHave(text("Войти"));
+
+        $("[name=email]").setValue("qagurubot@gmail.com");
+        $("[name=password]").setValue("qagurupassword").pressEnter();
+        $(".main-header__login").click();
+
+        $(".logined-form").shouldHave(text("Здравствуйте, QA_GURU_BOT"));
+
+
+    }
+}
---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,38 @@
+package junit;
+
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Это метод @BeforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Это метод  @AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("    Это метод @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("    Это метод @AfterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("       Это firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("       Это secondTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+ }
